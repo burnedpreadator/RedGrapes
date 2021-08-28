3ddc539add13a73c0c7055d27d5bd5c896c9c33a
burnedpredator <chhariavikram1@gmail.com>
2021-08-28 23:55:50 +0530

    overlay active class added

--- public/js/work.js
+++ public/js/work.js
@@ -12,3 +12,36 @@ $(function(){
     $('.target').show();
   })
 });
+
+const OpenInfoButtons = document.querySelectorAll('[data-info-target]')
+const CloseInfoButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('show')
+
+OpenInfoButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const info = document.querySelector(button.dataset.infoTarget)
+    openInfo(info)
+  })
+})
+
+
+CloseInfoButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const info = button.closest(".info")
+    closeInfo(info)
+  })
+})
+
+function openInfo(info){
+  if(info == null) return
+  info.classList.add('active')
+  overlay.classList.add('active')
+}
+
+
+function closeInfo(info){
+  if(info == null) return
+  info.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
